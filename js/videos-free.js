204ca33a4cbc49971c1bcef4b3b9df9c841b6ee9
Emercy2021 <145089445+Emercy2021@users.noreply.github.com>
2024-01-25 20:38:53 +0200

    Update videos-free.js

--- js/videos-free.js
+++ js/videos-free.js
@@ -17,11 +17,14 @@
  * ]),
  */
 const videosFree = [
-    ['videos/[Re-upload] [1080p] HONK HONK.mp4', 0, 25],
-    ['videos/[SFM] Shrekophone.mp4', 0, 32],
-    ['videos/01.mp4', 0, 30],
-    ['videos/02.mp4', 0, 28],
-    ['videos/03.mp4', 0, 27],
-    ['videos/04.mp4', 0, 28],
-    ['videos/06.mp4', 0, 27]
+    () => p5Wheel.random([
+        ['videos/[Re-upload] [1080p] HONK HONK.mp4', 0, 25],
+        ['videos/[SFM] Shrekophone.mp4', 0, 32],
+        ['videos/01.mp4', 0, 30],
+        ['videos/02.mp4', 0, 28],
+        ['videos/03.mp4', 0, 27],
+        ['videos/04.mp4', 0, 28],
+        ['videos/06.mp4', 0, 27]
+    ])
+    
 ];
