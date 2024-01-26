b21f2ef99cbf8faa88119ab30fadfe73dcfc6561
Emercy2021 <145089445+Emercy2021@users.noreply.github.com>
2024-01-26 19:04:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -111,15 +111,18 @@ editConfirmButton.addEventListener('click', function () {
 });
 
 const p5Wheel = new p5(WheelSketch);
+
+const DMCAPlaylistSwitcher = new CheckboxStateable('with-dmca', 'video-with-dmca-protection', CheckboxStateable.MODE_MERGE);
+
 DMCAPlaylistSwitcher
     .setValues(videosProtected, videosFree)
     .onSwitch((value) => {
-        p5Wheel.setVideo(new Video(value, ''));
+        p5Wheel.setVideo(new Video(value, 'whopg2-wheel/'));
     })
 ;
 
 p5Wheel.onAfterSetup = function () {
-    p5Wheel.setVideo(new Video(DMCAPlaylistSwitcher.value, ''));
+    p5Wheel.setVideo(new Video(DMCAPlaylistSwitcher.value, 'whopg2-wheel/'));
 };
 
 const image = document.querySelector('#item-image img');
