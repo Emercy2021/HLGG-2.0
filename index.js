f5003c5ae5f24b5310062089dba5f11ec9e562eb
Emercy2021 <145089445+Emercy2021@users.noreply.github.com>
2024-01-26 19:00:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -114,12 +114,12 @@ const p5Wheel = new p5(WheelSketch);
 DMCAPlaylistSwitcher
     .setValues(videosProtected, videosFree)
     .onSwitch((value) => {
-        p5Wheel.setVideo(new Video(value, 'whopg2-wheel/'));
+        p5Wheel.setVideo(new Video(value, ''));
     })
 ;
 
 p5Wheel.onAfterSetup = function () {
-    p5Wheel.setVideo(new Video(DMCAPlaylistSwitcher.value, 'whopg2-wheel/'));
+    p5Wheel.setVideo(new Video(DMCAPlaylistSwitcher.value, ''));
 };
 
 const image = document.querySelector('#item-image img');
